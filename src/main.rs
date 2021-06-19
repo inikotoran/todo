a9eec246cdaab30c9a30499edff00693065e3ea8
niko.juliyanto <niko.juliyanto@go-jek.com>
2021-06-19 20:16:35 +0700

    create task todo and loop stdin to create task

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,49 @@
-use std::env;
+use std::io::{self, BufRead};
+use std::fs::File;
+use std::io::prelude::*;
 
 mod todo;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut task1 = todo::Task::new_task(&args[1]);
-    println!("created a task: {}", task1.name());
-    task1.check();
-    println!("{} is {}", task1.name(), task1.status());
+    let mut my_todo = todo::Todo::new();
+    let stdin = io::stdin();
+    let iterator = stdin.lock().lines();
+    let mut is_checking = false;
+    for line in iterator {
+        let text = line.unwrap();
+        match text.as_ref() {
+            "status" => {
+                let tasks_status = my_todo.status();
+                for task_status in tasks_status {
+                    println!("{}", &task_status);
+                }
+            },
+            "check" => {
+                println!("choose index to check: ");
+                let tasks_status = my_todo.status();
+                for task_status in tasks_status {
+                    println!("{}", &task_status);
+                }
+                is_checking = true;
+            }
+            "write" => {
+                let mut file = File::create("todo.txt").unwrap();
+                let tasks_status = my_todo.status();
+                for task_status in tasks_status {
+                    file.write_all(format!("{}\n", &task_status).as_bytes()).expect("failed to write todo");
+                }
+            }
+            _ => {
+                if is_checking {
+                    let id_to_check = text.parse::<i32>().unwrap();
+                    my_todo.check(id_to_check);
+                    is_checking = false;
+                } else {
+                    let task = todo::Task::new_task(&text);
+                    my_todo.add_task(task);
+                    println!("{} added", text);
+                }
+            },
+        }
+    }
 }
--- src/todo.rs
+++ src/todo.rs
@@ -1,3 +1,44 @@
+pub struct Todo {
+    todo_tasks: Vec<TodoTask>,
+    task_id: i32,
+}
+
+pub struct TodoTask {
+    id: i32,
+    task: Task,
+}
+
+impl Todo {
+    pub fn new() -> Todo {
+        Todo{ todo_tasks: Vec::new(), task_id: 1 }
+    }
+
+    pub fn add_task(&mut self, task: Task) {
+        self.todo_tasks.push(TodoTask{task, id: self.task_id});
+        self.task_id += 1;
+    }
+    
+    pub fn status(&self) -> Vec<String> {
+        let mut status: Vec<String> = Vec::new();
+        for todo_task in &self.todo_tasks {
+            if todo_task.task.done {
+                status.push(format!("[x] {}. {}", todo_task.id, todo_task.task.name));
+            } else {
+                status.push(format!("[ ] {}. {}", todo_task.id, todo_task.task.name));
+            }
+        }
+        status
+    }
+
+    pub fn check(&mut self, id_to_check: i32) {
+        for todo_task in &mut self.todo_tasks {
+            if todo_task.id == id_to_check {
+                todo_task.task.done = true;
+            }
+        }
+    }
+}
+
 pub struct Task {
     name: String,
     done: bool,
@@ -6,20 +47,5 @@ pub struct Task {
 impl Task {
     pub fn new_task(name: &String) -> Task {
         Task{ name: name.to_string(), done: false}
-    }
-    
-    pub fn name(&self) -> &String {
-        &self.name
-    }
-
-    pub fn status(&self) -> String {
-        if self.done {
-            return String::from("done")
-        }
-        String::from("not done")
-    }
-    
-    pub fn check(&mut self) {
-        self.done = true
     }
 }
